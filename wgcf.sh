27d0b35d3565645b709184b5fc66dd88fe4cb69a
Guihong Wang <minecraftis1st@outlook.com>
2022-03-27 08:10:10 +0800

    Add files via upload

---
+++ wgcf.sh
@@ -0,0 +1,7 @@
+cd /gui
+mkdir wgcf
+cd wgcf
+wget https://git.gdwgh.workers.dev/ViRb3/wgcf/releases/download/v2.2.12/wgcf_2.2.12_linux_amd64
+cp wgcf_2.2.12_linux_amd64 wgcf
+chmod 777 wgcf
+echo 部分完成 可以 ./wgcf register 注册一个warp账号 也可以把你的wgcf-account.toml 复制到这里
