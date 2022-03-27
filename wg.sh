b4ea81fd77f2efec1de2a93bd688003ac7f52910
Guihong Wang <marisa@marisa.ml>
2022-03-27 16:26:14 +0800

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -9,7 +9,7 @@ ln -s /gui/go/bin/go /usr/local/bin/go
 cd
 echo 如果是-bash: go: command not found 提交issue if there has -bash: go: command not found please make a issue
 go version
-apt install make  curl wireguard-tools git -y
+apt install make unzip zip curl wireguard-tools git -y
 cd /tmp
 wget https://git.zx2c4.com/wireguard-go/snapshot/wireguard-go-0.0.20220316.zip
 unzip wireguard-go-0.0.20220316.zip
